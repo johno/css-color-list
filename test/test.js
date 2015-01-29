640400b19f27b48a0c13a02c30fba43ce60664b4
John Otander <johnotander@gmail.com>
2015-01-29 16:20:06 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssColorList = require('..');
+
+describe('css-color-list', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(cssColorList(), true);
+  });
+});
