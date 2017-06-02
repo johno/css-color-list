f889aa5b5c3b1c8286acdce8c418f8470a0f7048
John Otander <johnotander@gmail.com>
2017-06-02 09:25:23 -0600

    Cleanup module a bit

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-var assert = require('assert');
-var cssColorList = require('..');
+var assert = require('assert')
+var cssColorList = require('./')
 
 describe('css-color-list', function() {
-
   it('should include css colors', function() {
     ['silver', 'rebeccapurple', 'tomato'].forEach(function(color) {
-      assert.ok(cssColorList().indexOf(color) != -1);
-    });
-  });
+      assert.ok(cssColorList().indexOf(color) != -1)
+    })
+  })
 
   it('should not include unofficial colors', function() {
     ['puke', 'foo'].forEach(function(color) {
-      assert.ok(cssColorList().indexOf(color) == -1);
-    });
-  });
-});
+      assert.ok(cssColorList().indexOf(color) == -1)
+    })
+  })
+})
